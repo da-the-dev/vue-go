a15c086076dc3baf7719e13608f58fe6a4c700b6
da-the-dev <leshiktk@me.com>
2021-12-06 22:05:16 +0300

    Added some API calls

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import Cat from "./core/intefaces/cat";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
+  data() {
+    return {
+      cats: [] as Cat[],
+    };
+  },
+
+  created() {
+    console.log("here");
+    fetch("http://localhost:3000/api/cats/v1/get", {
+      method: "GET",
+    }).then((res) => {
+      res.json().then((r) => {
+        this.cats = r.cats;
+        console.log(this);
+      });
+    });
+  },
+
+  methods: {},
 });
 </script>
 
