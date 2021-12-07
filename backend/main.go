b889d08765ad05822f8130295c41cac1de2972eb
da-the-dev <leshiktk@me.com>
2021-12-07 10:40:11 +0300

    Added some v1 routing

--- backend/main.go
+++ backend/main.go
@@ -10,13 +10,16 @@ import (
 
 func main() {
 	router := mux.NewRouter()
-
 	router.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(rw).Encode("Hi")
 	}).Methods("GET")
 
-	router.HandleFunc("/posts", func(rw http.ResponseWriter, r *http.Request) {
-		json.NewEncoder(rw).Encode(db.DBRead())
+	v1 := router.PathPrefix("/api/v1").Subrouter()
+
+	v1.HandleFunc("/posts", func(rw http.ResponseWriter, r *http.Request) {
+		encoder := json.NewEncoder(rw)
+		encoder.SetIndent(" ", "    ")
+		encoder.Encode(db.DBRead())
 	}).Methods("GET")
 
 	println("Server started at port :3000. Connect: http://localhost:3000")
